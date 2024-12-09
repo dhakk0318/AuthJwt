2b87cf4769616641bd9b699d668b8483db2e9528
Shubham Daheriya <shubhamdaheriya1804@gmail.com>
2024-12-09 16:15:29 +0530

    second commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const verifyToken = require("../middleware/authMiddleware");
 const router = express.Router();
-
+ 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/refresh-token", userController.refreshToken);
